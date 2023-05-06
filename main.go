6b2e0d9f8469904f772d2076866dfb0e8896fb84
Kelvin Floresta <kelvin.floresta@frontdoorhome.com>
2023-05-06 14:49:04 -0300

    refactor: remove logic from main file

--- main.go
+++ main.go
@@ -4,15 +4,12 @@ import (
 	"fmt"
 	"goau-biat/commands"
 	"goau-biat/config"
-	"goau-biat/sounds"
 	"log"
 	"net"
-	"os"
 	"time"
 
 	"github.com/fatih/color"
 	"github.com/manifoldco/promptui"
-	hook "github.com/robotn/gohook"
 	"github.com/tatsushid/go-fastping"
 )
 
@@ -35,10 +32,10 @@ func main() {
 	switch command {
 	case "Loot":
 		log.Default().Println("Starting loot")
-		go loot()
+		go commands.StartHunt()
 	case "Rune":
 		log.Default().Println("Starting rune")
-		go startRune()
+		go commands.StartRune()
 	case "Monitor":
 		log.Default().Println("Starting Monitor")
 		go startMonitor()
@@ -47,79 +44,70 @@ func main() {
 	select {}
 }
 
-func startRune() {
-	events := hook.Start()
-	go commands.Rune()
-
-	for ev := range events {
-		commands.Exit(ev)
-	}
-}
-
-var paused = false
-
-func loot() {
-	events := hook.Start()
-	for ev := range events {
-		togglePause(ev)
-
-		if paused {
-			continue
-		}
-
-		commands.ScheduleTimer(ev)
-		commands.HelpLoot(ev)
-		commands.UltimateHealing(ev)
-		commands.SmarRune(ev)
-		commands.SafeMagicShield(ev)
-
-		if ev.Keychar == config.GetLoot {
-			commands.GetLoot(ev)
-		}
-
-		if ev.Keychar == config.Checklist {
-			sounds.PlaySound(sounds.CheckListGoannas)
-		}
-
-	}
-}
-
-func togglePause(ev hook.Event) {
-	if ev.Keychar == config.Pause || rune(ev.Rawcode) == config.Pause {
-		log.Default().Println("Pause Get Loot")
-		paused = !paused
-	}
-}
-
 func startMonitor() {
-	p := fastping.NewPinger()
+	gatewayPinger := newPinger(config.GatewayIP)
+	externalPinger := newPinger(config.ExternalIP)
 
-	err := p.AddIP("192.168.0.1")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	var gatewayLatancy time.Duration
+	var externalLatency time.Duration
+
+	var printer = newLatencyPrinter()
+
+	gatewayPinger.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		gatewayLatancy = rtt
+		fmt.Println()
+		fmt.Print(" Gateway ")
+		printer(gatewayLatancy)
+		fmt.Print("\n External ")
+		printer(externalLatency)
+		fmt.Print("\n\n")
 	}
-	info := color.New(color.FgBlue).SetWriter(log.Default().Writer())
-	warning := color.New(color.FgYellow).SetWriter(log.Default().Writer())
-	danger := color.New(color.FgRed).SetWriter(log.Default().Writer())
-	superDanger := color.New(color.BgHiRed).SetWriter(log.Default().Writer())
 
-	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
-		latency := rtt.Milliseconds()
+	externalPinger.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		externalLatency = rtt
+		fmt.Print(" Gateway ")
+		printer(gatewayLatancy)
+		fmt.Print("\n External ")
+		printer(externalLatency)
+		fmt.Print("\n\n")
+	}
+
+	gatewayPinger.RunLoop()
+	externalPinger.RunLoop()
+}
+
+func newLatencyPrinter() func(duration time.Duration) {
+	info := color.New(color.FgBlue).SetWriter(log.Writer())
+	warning := color.New(color.FgYellow).SetWriter(log.Writer())
+	danger := color.New(color.FgRed).SetWriter(log.Writer())
+	superDanger := color.New(color.BgHiRed).SetWriter(log.Writer())
+
+	return func(duration time.Duration) {
+		latency := duration.Milliseconds()
+
 		switch {
 		case latency < 10:
-			info.Printf("Good: %s \n", rtt)
+			info.Printf("%s", duration)
 
 		case latency > 20 && latency < 50:
-			warning.Printf("Warning: %s \n", rtt)
+			warning.Printf("%s", duration)
 
 		case latency > 50 && latency < 100:
-			danger.Printf("Danger: %s \n", rtt)
+			danger.Printf("%s", duration)
 
 		case latency > 100:
-			superDanger.Printf("Danger: %s \n", rtt)
+			superDanger.Printf("%s", duration)
 		}
 	}
 
-	p.RunLoop()
+}
+
+func newPinger(ip string) *fastping.Pinger {
+	p := fastping.NewPinger()
+
+	err := p.AddIP(ip)
+	if err != nil {
+		log.Default().Fatalln(err)
+	}
+	return p
 }
