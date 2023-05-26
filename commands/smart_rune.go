9ce72a16863bd33e04c081a574f86d1b3d881095
Kelvin Floresta <kelvin.floresta@frontdoorhome.com>
2023-05-26 17:21:37 -0300

    fix: remove conflict with others keys

--- commands/smart_rune.go
+++ commands/smart_rune.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"goau-biat/config"
+	"goau-biat/util"
 	"log"
 	"math/rand"
 	"time"
@@ -10,8 +11,8 @@ import (
 	hook "github.com/robotn/gohook"
 )
 
-func SmarRune(ev hook.Event) {
-	if ev.Keychar == config.AttackRuneKey {
+func SmartRune(ev hook.Event) {
+	if ev.Rawcode == config.AttackRuneKey && util.IsKeyDown(ev.Kind) {
 		delay := time.Millisecond * time.Duration(rand.Intn(100)+200)
 		time.Sleep(delay)
 		robotgo.Click()
